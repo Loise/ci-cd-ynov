d3c9b57ed698c4f1df149f5644d222764911c11f
Loïse Fenoll <myuuu@live.fr>
2025-03-25 11:36:27 +0100

    adding e2e tests and fix docker env

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,27 +1,17 @@
 describe('template spec', () => {
-  it('deployed react app to localhost', () => {
-    cy.intercept('GET', 'https://ci-cd-back-ynov.vercel.app/users', {
+  beforeEach(function () {
+    cy.fixture('employees').as('employeesJSON')
+  })
+  it('deployed react app to localhost', function(){
+    let users = this.employeesJSON;
+    //debugger
+    cy.intercept('GET', '/users', {
       statusCode: 200,
-      body: {
-        utilisateurs: [
-          {
-            id: '1',
-            nom: 'a',
-            prenom: 'b',
-            email: 'c@c.fr'
-          },
-          {
-            id: '2',
-            nom: 'a',
-            prenom: 'b',
-            email: 'c@c.fr'
-          }
-        ],
-      },
+      body: {...users},
     });
 
     cy.visit('http://localhost:3000')
-    cy.contains('2 user(s) already registered')
+    cy.contains('3 user(s) already registered')
   })
 
 })
